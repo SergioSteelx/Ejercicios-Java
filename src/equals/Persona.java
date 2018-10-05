0f742a573fd64e4a52218b13dba1939e0fcff015
Sergio Rivera <sergio_checo1211@hotmail.com>
2018-10-05 13:08:14 -0500

    Se agrega la clase persona

--- src/equals/Persona.java
+++ src/equals/Persona.java
@@ -1,4 +1,39 @@
 package equals;
 
-public class Persona {
+import java.util.Objects;
+
+public class Persona
+{
+    private String nombre;
+    private int edad;
+
+    public int getEdad() {
+        return edad;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Persona persona = (Persona) o;
+        return edad == persona.edad &&
+                Objects.equals(nombre, persona.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre, edad);
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
