44f681f23e625b471430a1f52d91c642bb3c761b
Sergio Rivera <sergio_checo1211@hotmail.com>
2018-10-08 11:47:09 -0500

    se invoca clase DVD y se instancia y se mandan parametros y se llaman por medio de get los valores de las variables y se muestran en consola

--- src/Colecciones/MainParaColecciones.java
+++ src/Colecciones/MainParaColecciones.java
@@ -1,4 +1,14 @@
 package Colecciones;
 
 public class MainParaColecciones {
+
+    public static void main(String[] args)
+    {
+        DVD dvd = new DVD("Titan AE","Animación, Ciencia ficción, Aventura, Acción","Cale Tucker");
+
+        System.out.println("Titulo: "+ dvd.getTitulo());
+        System.out.println("Genero: " + dvd.getGenero());
+        System.out.println("Protagonista: "+ dvd.getProtagonista());
+
+    }
 }
