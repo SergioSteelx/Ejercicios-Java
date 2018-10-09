f9de60ef9d2e7cbf745490441afe780a0887f5b8
Sergio Rivera <sergio_checo1211@hotmail.com>
2018-10-09 12:55:53 -0500

    Se agrega metodo comparable y se sobreescribe metodo

--- src/Colecciones/DVD.java
+++ src/Colecciones/DVD.java
@@ -1,13 +1,16 @@
 package Colecciones;
+import java.util.*;
 
-public class DVD {
+public class DVD implements Comparable<DVD>
+{
 
     private String Titulo;
     private String Genero;
     private String Protagonista;
+    private String Director;
 
     // constructor Default
-    public void DVD ()
+    public void DVD()
     {
     }
 
@@ -28,29 +31,59 @@ public class DVD {
         this(titulo, genero);
         this.Protagonista = protagonista;
     }
+    public DVD (String titulo, String genero, String protagonista, String director)
+    {
+        this(titulo, genero, protagonista);
+        this.Director = director;
+    }
 
-//--- get y set de titulo
-    public String getTitulo() {
+    @Override
+    public String toString() {
+        return "\n|------------------------------------------------------------------|\n|   Titulo:       |  "+ getTitulo()+ "\n|------------------------------------------------------------------|\n|   Genero:       |  "+ getGenero()+ "\n|------------------------------------------------------------------| \n|   Protagonista: |  "+getProtagonista()+"\n|------------------------------------------------------------------|";
+    }
+
+    @Override
+    public int compareTo(DVD o) {
+        return Titulo.compareTo(o.Titulo);
+    }
+
+
+    //--- get y set de titulo
+    public String getTitulo()
+    {
         return Titulo;
     }
 
-    public void setTitulo(String titulo) {
+    public void setTitulo(String titulo)
+    {
         this.Titulo = titulo;
     }
     //--- get y set genero
-    public String getGenero() {
+    public String getGenero()
+    {
         return Genero;
     }
 
-    public void setGenero(String genero) {
+    public void setGenero(String genero)
+    {
         Genero = genero;
     }
     //--- get y set de protaonista
-    public String getProtagonista() {
+    public String getProtagonista()
+    {
         return Protagonista;
     }
 
-    public void setProtagonista(String protagonista) {
+    public void setProtagonista(String protagonista)
+    {
         Protagonista = protagonista;
+    }
+
+    public String getDirector() {
+        return Director;
+    }
+
+    public void setDirector(String director) {
+        Director = director;
     }
 }
