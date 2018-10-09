fea195088f90181162a8054ca8830ca2a671fe54
Sergio Rivera <sergio_checo1211@hotmail.com>
2018-10-09 10:06:56 -0500

    Se agrego 4 for anidados para probar salida

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -78,5 +78,18 @@ public class HolaMundo
 
         //------------------------- equals entre variables -------------------------
 
+        for(int i = 0;i<10;i++)
+        {
+            for(int j = 0;j<10;j++)
+            {
+                for(int k = 0;k<10;k++)
+                {
+                    for(int l = 0;l<10;l++)
+                    {
+                        System.out.println(i+" , "+ j +" , "+ k +" , "+ l);
+                    }
+                }
+            }
+        }
     }
 }
