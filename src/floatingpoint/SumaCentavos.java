8161f94248bc8a76fe9bf3d1a5c5de6c97d08d80
Sergio Rivera <sergio_checo1211@hotmail.com>
2018-10-17 09:44:17 -0500

    se prueban la clase bigfloat

--- src/floatingpoint/SumaCentavos.java
+++ src/floatingpoint/SumaCentavos.java
@@ -1,4 +1,20 @@
 package floatingpoint;
 
-public class SumaCentavos {
+import java.math.BigDecimal;
+
+public class SumaCentavos
+{
+    public static void main(String[] args)
+    {
+
+        BigDecimal unCentavobigDecimal = new BigDecimal("0.01");
+        BigDecimal sumaBigDecimal = unCentavobigDecimal.add(unCentavobigDecimal).add(unCentavobigDecimal)
+                .add(unCentavobigDecimal)
+                .add(unCentavobigDecimal)
+                .add(unCentavobigDecimal);
+        System.out.println(sumaBigDecimal);
+        double unCentavo = 0.01;
+        double suma = unCentavo + unCentavo + unCentavo + unCentavo + unCentavo + unCentavo;
+        System.out.println(suma);
+    }
 }
