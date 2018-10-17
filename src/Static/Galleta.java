11ef2fe3939eed12e0186e7969054c9bb8bcc0d5
Sergio Rivera <sergio_checo1211@hotmail.com>
2018-10-17 09:41:45 -0500

    se hace galleta y se prueban cosas

--- src/Static/Galleta.java
+++ src/Static/Galleta.java
@@ -1,4 +1,163 @@
 package Static;
 
-public class Galleta {
+public class Galleta
+{
+
+
+
+    private String Nombre;
+    private String Tamanio;
+    private String Color;
+    private String Olor;
+    private String Sabor;
+    private String Relleno;
+    private String Forma;
+    private String grosor;
+    private static  int cantidad = 0;
+
+    //--------------------------------- constructores -----------------------------------
+    public Galleta()
+    {
+
+    }
+
+    public Galleta(String nombre)
+    {
+        this.Nombre = nombre;
+    }
+
+    public Galleta(String nombre,String tamanio)
+    {
+        this(nombre);
+        this.Tamanio = tamanio;
+    }
+
+
+    public Galleta(String nombre,String tamanio, String color)
+    {
+        this (nombre,tamanio);
+        this.Color = color;
+    }
+
+    public Galleta(String nombre,String tamanio, String color, String olor)
+    {
+        this (nombre,tamanio, color);
+        Olor = olor;
+    }
+
+    public Galleta(String nombre,String tamanio, String color, String olor,String sabor)
+    {
+        this (nombre,tamanio, color,olor);
+        this.Sabor = sabor;
+    }
+
+    public Galleta(String nombre,String tamanio, String color, String olor, String sabor, String relleno) {
+       this (nombre,tamanio, color, olor, sabor);
+        this.Relleno = relleno;
+    }
+
+    public Galleta(String nombre,String tamanio, String color, String olor, String sabor, String relleno, String forma) {
+        this (nombre,tamanio, color, olor, sabor, relleno);
+        this.Forma = forma;
+    }
+    public Galleta(String nombre,String tamanio, String color, String olor, String sabor, String relleno, String forma, String grosor) {
+        this (nombre,tamanio, color, olor, sabor, relleno, forma);
+        this.grosor = grosor;
+        cantidad = cantidad + 1;
+    }
+    public Galleta(int cantidad, String nombre, String tamanio, String color, String olor, String sabor, String relleno, String forma, String grosor) {
+        this (nombre, tamanio, color, olor, sabor, relleno, forma,grosor);
+        this.cantidad = cantidad;
+    }
+
+    //-----------------------------------geters y seter de variables----------------------------------------------
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.Nombre = nombre;
+    }
+
+    public String getTamanio() {
+        return Tamanio;
+    }
+
+    public void setTamanio(String tamanio) {
+        Tamanio = tamanio;
+    }
+
+    public String getColor() {
+        return Color;
+    }
+
+    public void setColor(String color) {
+        Color = color;
+    }
+
+    public String getOlor() {
+        return Olor;
+    }
+
+    public void setOlor(String olor) {
+        Olor = olor;
+    }
+
+    public String getSabor() {
+        return Sabor;
+    }
+
+    public void setSabor(String sabor) {
+        Sabor = sabor;
+    }
+
+    public String getRelleno() {
+        return Relleno;
+    }
+
+    public void setRelleno(String relleno) {
+        Relleno = relleno;
+    }
+
+    public String getForma() {
+        return Forma;
+    }
+
+    public void setForma(String forma) {
+        Forma = forma;
+    }
+
+    public String getGrosor() {
+        return grosor;
+    }
+
+    public void setGrosor(String grosor) {
+        this.grosor = grosor;
+    }
+    public static int getCantidad() {
+        return cantidad;
+    }
+
+    @Override
+    public String toString() {
+
+        return"\n|------------------------------------------------------------------|\n" +
+                "|   Numero:      |  "+ getCantidad()+ "\n" +
+                "|----------------|-------------------------------------------------|\n" +
+                "|   Tamaño:      |  "+ getTamanio()+ "\n" +
+                "|----------------|-------------------------------------------------|\n" +
+                "|   Color:       |  "+ getColor()+ "\n" +
+                "|----------------|-------------------------------------------------| \n" +
+                "|   Olor:        |  "+ getOlor()+"\n" +
+                "|----------------|-------------------------------------------------| \n" +
+                "|   Sabor:       |  "+ getSabor()+"\n" +
+                "|----------------|-------------------------------------------------| \n" +
+                "|   Relleno:     |  "+ getRelleno()+"\n" +
+                "|----------------|-------------------------------------------------| \n" +
+                "|   Forma:       |  "+ getForma()+"\n" +
+                "|----------------|-------------------------------------------------| \n" +
+                "|   Grosor:      |  "+ getGrosor()+"\n" +
+                "|------------------------------------------------------------------| ";
+
+    }
 }
--- src/Static/GalletaMain.java
+++ src/Static/GalletaMain.java
@@ -1,4 +1,58 @@
 package Static;
+import java.util.*;
 
-public class GalletaMain {
+
+public class GalletaMain
+{
+    public static void main(String[] args)
+    {
+        Galleta galletas = new Galleta();
+        List<Galleta> galleton = Listagalleta();
+
+        for (Galleta galleta:galleton)
+        {
+            System.out.println(galleta);
+            //System.out.println(galletas.getCantidad());
+
+
+        }
+
+    }
+    public static List<Galleta> Listagalleta ()
+    {
+
+        Galleta chocolate = new Galleta("triki-trakes","Chica", "Negro", "chocolate", "chocolate", "Sin Relleno", "Cuadrada", "medio");
+        Galleta Naranja = new Galleta("Polovornes","Chica", "Amarilla", "chocolate", "chocolate", "Sin Relleno", "Cuadrada", "medio");
+        Galleta Limon = new Galleta("Piruetas","Chica", "Amarilla", "chocolate", "chocolate", "Sin Relleno", "Cuadrada", "medio");
+        Galleta vainilla = new Galleta("Marias","Chica", "Amarilla", "Vainilla", "Vainilla", "Sin Relleno", "Cuadrada", "medio");
+        Galleta conChispas = new Galleta("Chokis","Chica", "Amarilla con chispas de chocolate", "chocolate", "chocolate", "Sin Relleno", "Cuadrada", "medio");
+        Galleta chocolateBlanco = new Galleta("Marloleado","Chica", "Marmol", "Chocolate", "Chocolate", "Cuadrada", "medio");
+        Galleta Avena = new Galleta("Quaker","Chica", "Amarilla", "Vainilla", "Avena", "Sin Relleno", "Cuadrada", "medio");
+        Galleta DobleChocolate = new Galleta("Chokis Chocolaton","Chica", "Cafe", "chocolate", "chocolate", "Con Relleno", "Cuadrada", "medio");
+        Galleta Arandano = new Galleta("Quaker-A","Chica", "Amarilla", "Vainilla", "Arandano", "Sin Relleno", "Cuadrada", "medio");
+        Galleta Canela = new Galleta("Ricanela","Chica", "Cafe", "Canela", "Canela", "Sin Relleno", "Cuadrada", "medio");
+        Galleta animalitos = new Galleta("Animalitos","Chica", "Amarilla", "Vainilla", "Chocolate", "Sin Relleno", "Animal", "medio");
+        Galleta fresa = new Galleta("Florentinas","Chica", "Rosa", "Fresa", "Fresa", "Sin Relleno", "Cuadrada", "medio");
+        Galleta piruetas = new Galleta("Piruetas","Chica", "Amarilla", "Limon", "Limon", "Con Relleno", "Cuadrada", "medio");
+        Galleta oreo = new Galleta("Oreo","Chica", "Amarilla", "chocolate", "chocolate", "Con Relleno", "Cuadrada", "medio");
+
+
+        List<Galleta> dvds = new ArrayList<Galleta>();
+        dvds.add(chocolate);
+        dvds.add(Naranja);
+        dvds.add(Limon);
+        dvds.add(vainilla);
+        dvds.add(conChispas);
+        dvds.add(chocolateBlanco);
+        dvds.add(Avena);
+        dvds.add(DobleChocolate);
+        dvds.add(Arandano);
+        dvds.add(Canela);
+        dvds.add(animalitos);
+        dvds.add(fresa);
+        dvds.add(piruetas);
+        dvds.add(oreo);
+
+        return dvds;
+    }
 }
